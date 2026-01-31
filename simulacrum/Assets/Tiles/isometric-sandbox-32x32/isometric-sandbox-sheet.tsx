fca387b19ac47cd3e445f60c47d67e5380f30ce6
Owen Merkling <4662355+omerkling@users.noreply.github.com>
2026-01-31 13:14:23 -0600

    Add a door mechanic test scene
    Adjust collision shapes

--- simulacrum/Assets/Tiles/isometric-sandbox-32x32/isometric-sandbox-sheet.tsx
+++ simulacrum/Assets/Tiles/isometric-sandbox-32x32/isometric-sandbox-sheet.tsx
@@ -4,57 +4,57 @@
  <image source="isometric-sandbox-sheet.png" width="640" height="320"/>
  <tile id="66">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="-8.06929" y="22.9074">
-    <polygon points="-0.852765,0.170553 2.72885,-11.5976 15.1792,-15.0087 31.8081,1.62025 28.8235,13.4737 15.8614,17.0553"/>
+   <object id="1" x="8.625" y="39.625">
+    <polygon points="0,0 11.125,-2.625 15.125,-14.625 3.5,-11.25"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="84">
   <objectgroup draworder="index" id="4">
-   <object id="12" x="-7.55763" y="22.3957">
-    <polygon points="-0.596935,0.767488 6.65157,-22.0013 18.505,-26.3504 35.3897,-9.8068 27.5443,14.2412 16.1173,17.4817"/>
+   <object id="14" x="8.75" y="39.5">
+    <polygon points="0,0 7.25,-22.25 18.75,-26.75 11.625,-3.375"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="85">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="3.52831" y="19.3258">
-    <polygon points="0,0 8.18654,-24.7302 19.9547,-27.2885 36.7542,-10.6596 28.5676,12.5356 16.4584,16.6289"/>
+   <object id="2" x="26.75" y="30">
+    <polygon points="-5.5,5.5 2.125,-16.875 13.25,-21 5.375,2.125"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="86">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="4.38108" y="18.9847">
-    <polygon points="-0.852765,0.170553 2.72885,-11.5976 15.1792,-15.0087 31.8081,1.62025 28.8235,13.4737 15.8614,17.0553"/>
+   <object id="1" x="20.875" y="35.375">
+    <polygon points="-0.125,0.125 11.125,-2.875 15,-15 3.25,-11.25"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="103">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="8.6449" y="7.04597">
-    <polygon points="-0.852765,0.170553 2.72885,-11.5976 15.1792,-15.0087 31.8081,1.62025 28.8235,13.4737 15.8614,17.0553"/>
+   <object id="1" x="24.875" y="23.625">
+    <polygon points="0,0 10.875,-3.375 15.25,-14.75 3.875,-11.125"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="104">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="-8.23984" y="23.2485">
-    <polygon points="0,0 3.75217,-12.962 27.2032,-20.04 44.5996,-2.81412 40.9327,9.20986 16.97,17.3111"/>
+   <object id="2" x="8.625" y="39.625">
+    <polygon points="0,0 22.5,-7 27.125,-18.375 4.625,-10.875"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="105">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="-4.06129" y="11.2245">
-    <polygon points="0,0 3.75217,-12.962 27.2032,-20.04 44.5996,-2.81412 40.9327,9.20986 16.97,17.3111"/>
+   <object id="2" x="13.125" y="27.625">
+    <polygon points="0,0 22.5,-7.25 26.875,-19.125 5.75,-12"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="106">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="-3.46436" y="10.9687">
-    <polygon points="-0.852765,0.170553 2.72885,-11.5976 15.1792,-15.0087 31.8081,1.62025 28.8235,13.4737 15.8614,17.0553"/>
+   <object id="1" x="12.875" y="28.125">
+    <polygon points="0,0 11.25,-4 15.125,-15.125 4.625,-10.375"/>
    </object>
   </objectgroup>
  </tile>
