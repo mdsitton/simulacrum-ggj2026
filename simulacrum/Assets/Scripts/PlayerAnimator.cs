06d0ab4dacb7d5d2e1456b7cef929a21dc09b821
Matthew Sitton <matthewsitton@gmail.com>
2026-01-31 10:53:32 -0600

    Further work on sprite animation scripts

--- simulacrum/Assets/Scripts/PlayerAnimator.cs
+++ simulacrum/Assets/Scripts/PlayerAnimator.cs
@@ -16,15 +16,55 @@ public enum CharacterDirection
     Right
 }
 
-public class PlayerAnimator
+public class PlayerAnimator : MonoBehaviour
 {
+    [SerializeField]
     private PlayerSpriteSet spriteSet;
 
     private CharacterState state;
     private CharacterDirection direction;
 
-    public PlayerAnimator(PlayerSpriteSet spriteSet)
+    private bool stateResetNeeded = false;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
     {
-        this.spriteSet = spriteSet;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void SetState(CharacterState newState)
+    {
+        state = newState;
+        stateResetNeeded = true;
+        UpdateSprite();
+    }
+
+    public void SetDirection(CharacterDirection newDirection)
+    {
+        direction = newDirection;
+        stateResetNeeded = true;
+        UpdateSprite();
+    }
+
+    int frameIndex = 0;
+    int frameCount = 0;
+
+    private void UpdateSprite()
+    {
+        (Sprite sprite, var newFrameCount) = spriteSet.GetSprite(state, direction, frameIndex);
+        frameCount = newFrameCount;
+        if (stateResetNeeded)
+        {
+            frameIndex = 0;
+            stateResetNeeded = false;
+        }
+        spriteRenderer.sprite = sprite;
+    }
+
+    private void Update()
+    {
+        frameIndex = (frameIndex + 1) % frameCount;
+        UpdateSprite();
     }
 }
--- simulacrum/Assets/Scripts/PlayerSpriteSet.cs
+++ simulacrum/Assets/Scripts/PlayerSpriteSet.cs
@@ -41,4 +41,56 @@ public class PlayerSpriteSet : ScriptableObject
     Sprite[] walkingLeft;
     [SerializeField]
     Sprite[] walkingRight;
+
+    public (Sprite sprite, int frameCount) GetSprite(CharacterState state, CharacterDirection direction, int frameIndex = 0)
+    {
+        switch (state)
+        {
+            case CharacterState.Standing:
+                var spriteStand = direction switch
+                {
+                    CharacterDirection.Front => standingFront,
+                    CharacterDirection.Back => standingBack,
+                    CharacterDirection.Left => standingLeft,
+                    CharacterDirection.Right => standingRight,
+                    _ => throw new System.NotImplementedException()
+                };
+                return (spriteStand, 1);
+
+            case CharacterState.Sitting:
+                var spriteSit = direction switch
+                {
+                    CharacterDirection.Front => sittingFront,
+                    CharacterDirection.Back => sittingBack,
+                    CharacterDirection.Left => sittingLeft,
+                    CharacterDirection.Right => sittingRight,
+                    _ => throw new System.NotImplementedException(),
+                };
+                return (spriteSit, 1);
+            case CharacterState.Die:
+                var spriteDie = direction switch
+                {
+                    CharacterDirection.Front => dieFront,
+                    CharacterDirection.Back => dieFront,
+                    CharacterDirection.Left => dieLeft,
+                    CharacterDirection.Right => dieLeft,
+                    _ => throw new System.NotImplementedException(),
+                };
+                return (spriteDie, 1);
+
+            case CharacterState.Walking:
+                var spriteAnimation = direction switch
+                {
+                    CharacterDirection.Front => walkingFront,
+                    CharacterDirection.Back => walkingBack,
+                    CharacterDirection.Left => walkingLeft,
+                    CharacterDirection.Right => walkingRight,
+                    _ => throw new System.NotImplementedException(),
+                };
+
+                return (spriteAnimation[frameIndex % spriteAnimation.Length], spriteAnimation.Length);
+        }
+
+        return (null, 0); // Default case if no sprite is found
+    }
 }
